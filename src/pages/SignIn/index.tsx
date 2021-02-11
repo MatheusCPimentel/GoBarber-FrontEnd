b8da0e7f67169d6c50e7e20fcf2135ba7a47264d
Matheus Pimentel <matheus08.pimentel@gmail.com>
2021-02-11 09:59:39 -0300

    Create isolated components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <form>
-        <h1>Faça seu logon</h1>
+        <form>
+          <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-        <input type="password" placeholder="Senha" />
+          <Button type="submit">Entrar</Button>
 
-        <button type="submit">Entrar</button>
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
-
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+        <a href="temp">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
